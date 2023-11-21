0af7d0d28defecbb9b50feb3a88caa2fdc3b5c23
Amith639 <amithemanth4@gmail.com>
2023-11-21 16:55:20 +0530

    Adding the lodash npm

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+var _ = require('lodash');
+console.log(_);
+
+var array1 = [1,2,3,4,5,6,7,8];
+console.log('answer',_.without(array1,3));
+
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
